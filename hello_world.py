b031fbf94528ea2e5d81afc4a8c840c27c714595
s2010595014 <95210174+s2010595014@users.noreply.github.com>
2022-01-18 11:05:02 +0100

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -3,5 +3,5 @@ from time import sleep
 # the program will print hello world
 #  every 1 second foever
 while True:
-    print("Hello, World")
-    sleep(1)
+    print("Hello, World - CLC")
+    sleep(5)
